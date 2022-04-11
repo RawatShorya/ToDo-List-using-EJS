f6ad051204afe8115190c24ceb9b15543368788f
RawatShorya <90060588+RawatShorya@users.noreply.github.com>
2022-04-12 00:30:20 +0530

    ✨ Added get and post routes

--- app.js
+++ app.js
@@ -6,3 +6,16 @@ const _ = require("lodash");
 const app = express();
 
 app.set("view engine", "ejs");
+
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+app.get("/", function (req, res) {});
+
+app.post("/", function (req, res) {});
+
+app.listen(3000, function () {
+  console.log("Server Has Started On Port 3000.");
+});
