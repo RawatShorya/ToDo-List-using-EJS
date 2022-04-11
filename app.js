e5c3cb920191c21f18060c1dc91c0b2268d2c171
RawatShorya <90060588+RawatShorya@users.noreply.github.com>
2022-04-12 00:48:12 +0530

    ✨ added schema for items

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyparser = require("body-parser");
 const _ = require("lodash");
+const { stringify } = require("querystring");
 
 const app = express();
 
@@ -10,7 +11,39 @@ app.set("view engine", "ejs");
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+//Connection to Database Server
 mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+//Creating a Schema
+const itemsSchema = {
+  name: string,
+};
+
+//Creating a mongoose model
+const Item = mongoose.model("Item", itemsSchema);
+
+//Creating some random items for todo list root route
+const item1 = new Item({
+  name: "Hola",
+});
+
+const item2 = new Item({
+  name: "Hit + to add a work",
+});
+
+const item3 = new Item({
+  name: "Click checkbox to delete",
+});
+
+const defaultItems = [item1, item2, item3];
+
+//Creating Schema for immediate routes
+const listSchema = {
+  name: String,
+  items: [itemsSchema],
+};
+
+const List = mongoose.model("List", listSchema);
 
 app.get("/", function (req, res) {});
 
